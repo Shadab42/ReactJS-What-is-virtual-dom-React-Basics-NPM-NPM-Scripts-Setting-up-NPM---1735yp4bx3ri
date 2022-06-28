f5bfb75fcac9004d898f6594b14a3d2b99738310
Shadab42 <shadabmalikthanvi42@gmail.com>
2022-06-28 10:48:21 +0530

    finish

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,10 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-      <h1 value="project-name">project-name</h1>
-      <p>project-description</p>
+      <h1 data-ns-test="project-name"> Online Food Selling App </h1>
+      <dl data-ns-test="project-description">
+        <dd>Selling Sea food, fishes and meats in online </dd>
+      </dl>
     </div>
   )
 }
